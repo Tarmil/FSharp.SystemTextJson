0f5ffba4b6cb7b384f770c6f91d1b4b39dea1286
Loic Denuziere <loic@denuziere.net>
2019-08-07 00:50:40 +0200

    Remove push to nuget.org from AppVeyor (use AV Deployments instead)

--- build.fsx
+++ build.fsx
@@ -3,7 +3,6 @@ nuget FSharp.Core ~> 4.6.0
 nuget Fake.DotNet.Cli
 nuget Fake.IO.FileSystem
 nuget Fake.Core.Target
-nuget System.Text.Json preview7
 //"
 #load ".fake/build.fsx/intellisense.fsx"
 #nowarn "52"
@@ -15,7 +14,6 @@ open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
-open System.Text.Json
 
 let ctx = Context.forceFakeContext()
 
@@ -30,14 +28,11 @@ module Cli =
 
     let clean = hasFlag ["-c"; "--clean"] ctx.Arguments
     let pushTestsUrl = getOpt ["--push-tests"] ctx.Arguments
-    let avToken = getOpt ["--av-token"] ctx.Arguments
-    let nugetKey = getOpt ["--nuget-key"] ctx.Arguments
 
 module Paths =
     let root = __SOURCE_DIRECTORY__
     let sln = root </> "FSharp.SystemTextJson.sln"
     let out = root </> "bin"
-    let nugetTemp = out </> "from-av"
 
 Target.create "Clean" (fun _ ->
     !! "**/bin"
@@ -75,50 +70,6 @@ Target.create "Test" (fun _ ->
 )
 
 Target.create "All" ignore
-
-let downloadAppVeyorArtifacts (project: string) (targetDir: string) (token: string) =
-    use c = new WebClient()
-    let setAuth() =
-        c.Headers.Add("Authorization", "Bearer " + token)
-
-    setAuth()
-    c.Headers.Add("Content-type", "application/json")
-    let s = c.DownloadString(sprintf "https://ci.appveyor.com/api/projects/%s" project)
-    let jobId = JsonDocument.Parse(s).RootElement.GetProperty("build").GetProperty("jobs").[0].GetProperty("jobId").GetString()
-
-    setAuth()
-    c.Headers.Add("Content-type", "application/json")
-    let s = c.DownloadString(sprintf "https://ci.appveyor.com/api/buildjobs/%s/artifacts" jobId)
-    let artifacts = JsonDocument.Parse(s).RootElement
-
-    artifacts.EnumerateArray()
-    |> Seq.map (fun artifact ->
-        let fileName = artifact.GetProperty("fileName").GetString()
-        printfn "Downloading %s from AppVeyor..." fileName
-        setAuth()
-        let url = sprintf "https://ci.appveyor.com/api/buildjobs/%s/artifacts/%s" jobId fileName
-        let path = targetDir </> fileName
-        Directory.create (Path.getDirectory path)
-        c.DownloadFile(url, path)
-        printfn "Done."
-        path
-    )
-    |> Array.ofSeq
-
-Target.description "Push the latest CI build to nuget.org"
-Target.create "Push-Latest" (fun _ ->
-    Shell.cleanDir Paths.nugetTemp
-    Cli.avToken
-    |> Option.defaultWith (fun () -> failwith "Missing argument: --av-token <appveyor-token>")
-    |> downloadAppVeyorArtifacts "Tarmil/fsharp-systemtextjson" Paths.nugetTemp
-    |> Array.iter (DotNet.nugetPush (fun o ->
-        o.WithPushParams(
-            { o.PushParams with
-                ApiKey = Cli.nugetKey
-                Source = Some "https://api.nuget.org/v3/index.json"
-            })
-    ))
-)
 
 "Build"
 ==> "Test"
